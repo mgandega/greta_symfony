1ab94d505186a755553a702194a63fb49e648520
Mouhamed Gandega <mgandega@gmail.com>
2024-05-03 10:40:41 +0200

    crud avec php form

--- src/Controller/ConferencsController.php
+++ src/Controller/ConferencsController.php
@@ -12,47 +12,24 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ConferencsController extends AbstractController
 {
-    // public $conferences = [
-    //     [
-    //         'id' => 1,
-    //         'titre' => 'conference Symfony',
-    //         'description' => 'Conférence où on parlera des evênements et des subscribers',
-    //         'lieu' => 'paris',
-    //         "date" => "24-05-2024"
-    //     ],
-    //     [
-    //         'id' => 2,
-    //         'titre' => 'conference Laravel',
-    //         'description' => 'envoi des notifications à temps reels avec symfony messenger',
-    //         'lieu' => 'lyon',
-    //         "date" => "18-06-2024"
-    //     ],
-    //     [
-    //         'id' => 3,
-    //         'titre' => 'conference drupal',
-    //         'description' => 'comment installer des modules drupal avec symfony',
-    //         'lieu' => 'paris',
-    //         "date" => "21-12-2024"
-    //     ]
-    // ];
-    // #[Route('/conferences/{ville}/{id1}/{id2}', name: 'app_conferences', defaults: ['id1'=>'5'], requirements: ['ville'=>'[a-zA-Z]+', 'id2'=>'\d+'])]
-    // public function index($id1):Response
-    // {
-    //    return new Response('le numéro '.$id1);
-    // }
+  
+    public $em;
+    public function __construct(EntityManagerInterface $manager){
+        $this->em = $manager;
+    }
 
     // le nom d'une route doit être unique
     #[Route('/conferences', name: 'conference.index')]
-    public function index(EntityManagerInterface $manager): Response
+    public function index(): Response
     {
         // $manager->getRepository(Conference::class)->findAll() permet de recuperer toutes les conférences
-     $conferences = $manager->getRepository(Conference::class)->findAll();
+     $conferences = $this->em->getRepository(Conference::class)->findAll();
      // ici on retourne le template conferences.html.twig par rapport aux parametres passés en arguments
      return $this->render("conferences/conferences.html.twig", ['conferences' => $conferences]);
     }
 
     #[Route('/conference/details/{id}', name: 'conference.details', requirements: ['id' => '\d+'])]
-    public function details(EntityManagerInterface $manager, $id)
+    public function details($id)
     {
         // foreach ($this->conferences as $cle => $conference) {
         //     if ($conference['id'] == $id) {
@@ -60,13 +37,13 @@ class ConferencsController extends AbstractController
         //     }
         // }
 
-        $conference = $this->manager->getRepository(Conference::class)->find($id);
+        $conference = $this->em->getRepository(Conference::class)->find($id);
 
         return $this->render("conferences/conference.html.twig", ['conference' => $conference]);
     }
 
     #[Route('/conference/edit/{id}', name: 'conference.edit')]
-    public function editer(EntityManagerInterface $manager, $id)
+    public function editer($id)
     {
         // foreach ($this->conferences as $cle => $conference) {
         //     if ($conference['id'] == $id) {
@@ -74,48 +51,38 @@ class ConferencsController extends AbstractController
         //         $conferences[] = $conference;
         //     }
         // }
-        $conference = $manager->getRepository(Conference::class)->find($id);
+        $conference = $this->em->getRepository(Conference::class)->find($id);
         $conference->setTitre('teste de titre');
         // on utilise seulement le flush() si on veut supprimer ou modifier
         // si on utilise persist() et flush(), on rajoutera une autre ligne dans la table
-        $manager->flush();
+        $this->manager->flush();
         
         return $this->redirectToRoute('conference.index'); 
     }
     #[Route('/conference/supp/{id}', name: 'conference.supp')]
-    public function delete(EntityManagerInterface $manager, $id)
+    public function delete($id)
     {
-        $conference = $manager->getRepository(Conference::class)->find($id);
-        $manager->remove($conference); // pour supprimer
-        $manager->flush();
+        $conference = $this->em->getRepository(Conference::class)->find($id);
+        $this->em->remove($conference); // pour supprimer
+        $this->em->flush();
         return $this->redirectToRoute('conference.index'); 
     }
 
     #[Route('/conference/add', name: 'conference.add')]
-    public function add(EntityManagerInterface $manager)
+    public function add()
     {
-
-        $conference1 = new Conference();
-        $conference1->setTitre('conference Symfony')
-            ->setDescription('Conférence où on parlera des evênements et des subscribers')
-            ->setLieu('paris');
-        $conference2 = new Conference();
-        $conference2->setTitre('conference Laravel')
-            ->setDescription('envoi des notifications à temps reels avec symfony messenger')
-            ->setLieu('lyon');
-        $conference3 = new Conference();
-        $conference3->setTitre('conference drupal')
-            ->setDescription('comment installer des modules drupal avec symfony')
-            ->setLieu('paris');
-
-        $manager->persist($conference1);
-        $manager->persist($conference2);
-        $manager->persist($conference3);
-        $manager->flush();
-
+        // si on a cliqué sur le bouton submit (cela veut dire que le tableau n'est pas vide)
+        // si l'utilisateur à posté
+         if(!empty($_POST)){
+            $conference = new Conference();
+            $conference->setTitre($_POST['titre'])
+                ->setDescription($_POST['description'])
+                ->setLieu($_POST['lieu']);
+            $this->em->persist($conference); // enregistrer
+            $this->em->flush(); // valider l'enregistrement
+        }else{
+            return $this->render("conferences/formulaire.html.twig");
+        }
         return $this->redirectToRoute("conference.index");
     }
-
-
-
 }
