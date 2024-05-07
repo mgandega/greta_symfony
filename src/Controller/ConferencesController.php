133f28b4bd8964fc7239196f8a835f75b5c19e60
Mouhamed Gandega <mgandega@gmail.com>
2024-05-07 09:12:56 +0200

    ajouter de filtres et fonctions ...

--- src/Controller/ConferencesController.php
+++ src/Controller/ConferencesController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use DateTimeImmutable;
+use App\Entity\Categorie;
 use App\Entity\Conference;
 use App\Form\ConferenceType;
 use App\Repository\CategorieRepository;
@@ -12,6 +13,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class ConferencesController extends AbstractController
 {
@@ -24,12 +26,20 @@ class ConferencesController extends AbstractController
 
     // le nom d'une route doit être unique
     #[Route('/conferences', name: 'conference.index')]
-    public function index(): Response
+    #[Route('/conferences/categorie/{id}', name: 'conference.categorie')]
+    public function index(Request $request, CategorieRepository $categorie): Response
     {
+        // dd($request->attributes->get('id'));
+
         // $manager->getRepository(Conference::class)->findAll() permet de recuperer toutes les conférences
-        $conferences = $this->em->getRepository(Conference::class)->findAll();
+        if ($request->attributes->get('id')) {
+            $conferences = $this->em->getRepository(Conference::class)->findBy(['categorie' => $request->attributes->get('id')]);
+        } else {
+            $conferences = $this->em->getRepository(Conference::class)->findAll();
+        }
         // ici on retourne le template conferences.html.twig par rapport aux parametres passés en arguments
-        return $this->render("conferences/conferences.html.twig", ['conferences' => $conferences]);
+        $categories = $categorie->findAll();
+        return $this->render("conferences/conferences.html.twig", ['conferences' => $conferences, 'categories' => $categories]);
     }
 
     #[Route('/conference/details/{id}', name: 'conference.details', requirements: ['id' => '\d+'])]
@@ -55,22 +65,23 @@ class ConferencesController extends AbstractController
         //         $conferences[] = $conference;
         //     }
         // }
+
         $conference = $this->em->getRepository(Conference::class)->find($id);
 
-        $form = $this->createForm(ConferenceType::class, $conference,['button_label'=>'Modifier une conférence']);
+        $form = $this->createForm(ConferenceType::class, $conference, ['button_label' => 'Modifier une conférence']);
         $form->handleRequest($request);
         if ($form->isSubmitted() and $form->isvalid()) {
-            $conference = $form->get('titre')->getData();
+            $conference = $form->getData();
             // on utilise seulement le flush() si on veut supprimer ou modifier
             // si on utilise persist() et flush(), on rajoutera une autre ligne dans la table
             $this->em->flush();
 
             return $this->redirectToRoute('conference.index');
         }
-        return $this->render("conferences/edit.html.twig", ['form' => $form->createView(),'bouton'=>'modifier']);
+        return $this->render("conferences/edit.html.twig", ['form' => $form->createView(), 'bouton' => 'modifier']);
     }
     #[Route('/conference/supp/{id}', name: 'conference.supp')]
-    public function delete ( EntityManagerInterface $manager, $id)
+    public function delete($id)
     {
         $conference = $this->em->getRepository(Conference::class)->find($id);
         $this->em->remove($conference); // pour supprimer
@@ -81,6 +92,9 @@ class ConferencesController extends AbstractController
     #[Route('/conference/add', name: 'conference.add')]
     public function add(Request $request)
     {
+        // dd($_SERVER['DOCUMENT_ROOT'].'uploads/images');
+        $dossier_images = $_SERVER['DOCUMENT_ROOT'].'uploads/images';
+
         // si on a cliqué sur le bouton submit (cela veut dire que le tableau n'est pas vide)
         // si l'utilisateur à posté
         //  if(!empty($_POST)){
@@ -95,27 +109,40 @@ class ConferencesController extends AbstractController
         // }
 
         $conference = new Conference();
-        $form = $this->createForm(ConferenceType::class, $conference,['button_label'=>'Ajouter une conférence']);
+        $form = $this->createForm(ConferenceType::class, $conference, ['button_label' => 'Ajouter une conférence']);
 
         // ici je lie les données du formulaire avec l'objet conference s'il y'en a
         // il hydrate les propriétés
         $form->handleRequest($request);
         if ($form->isSubmitted() and $form->isvalid()) {
+            // dd($form->get('file')->getData()->getClientOriginalName());
+            // dd($form->getData()->getImage()->getFile()->getClientOriginalName());
+            $imagefile = $form->getData()->getImage()->getFile();
+            $imageName = $form->getData()->getImage()->getFile()->getClientOriginalName();
+            $imagefile->move($dossier_images, $imageName);
+            $fichier_bd = $dossier_images.'/'.$imageName;
             $conference = $form->getData();
+            $conference->getImage()->setFile($imagefile);
+            $conference->getImage()->setUrl($fichier_bd);
+            $conference->getImage()->setAlt($imageName);
             $this->em->persist($conference);
             $this->em->flush();
-            $this->addFlash('success','conférence ajouté avec succès');
+
             return $this->redirectToRoute("conference.index");
         }
-        return $this->render("conferences/ajout.html.twig", ['form' => $form->createView()]);
-    }
 
-    #[Route('/conferences/menu', name: 'menu')]
-    public function menu(ConferenceRepository $conference, CategorieRepository $categorie)
+        return $this->render("conferences/ajout.html.twig", ['form' => $form->createView(), 'bouton' => 'ajouter']);
+    }
+    // #[Route('/conferences/categorie/{id}', name:'conference.categorie')]
+    // public function conferencesParCategorie(ConferenceRepository $conference, $id){
+    //     $conferences = $conference->findBy(['categorie'=>$id]);
+    //     return $this->render("conferences/conferences.html.twig",['conferences'=>$conferences]);
+    // }
+    #[Route('/conferences/menu', name: 'conference.menu')]
+    public function menu()
     {
-        // $conferences = $this->em->getRepository(Conference::class)->findAll();
-        $conferences = $conference->findAll();
-        $categories = $categorie->findAll();
+        $conferences = $this->em->getRepository(Conference::class)->findAll();
+        $categories = $this->em->getRepository(Categorie::class)->findAll();
         return $this->render("conferences/menu.html.twig", ['conferences' => $conferences, 'categories' => $categories]);
     }
 }
