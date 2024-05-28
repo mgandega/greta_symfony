c27f7b01dc386455dfae92aacf6def4f340ca6a6
Mouhamed Gandega <mgandega@gmail.com>
2024-05-28 10:31:08 +0200

    reservation Many to One

--- src/Controller/ReservationController.php
+++ src/Controller/ReservationController.php
@@ -2,9 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Conference;
 use App\Entity\Reservation;
 use App\Form\ReservationType;
-use App\Repository\ConferenceRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,12 +14,29 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class ReservationController extends AbstractController
 {
     #[Route('/reservation/{id}', name: 'app_reservation')]
-    public function index(ConferenceRepository $conferenceRepository, EntityManagerInterface $manager, Request $request, $id): Response
+    public function index(EntityManagerInterface $manager, Request $request,$id): Response
     {
-        // return new Response($id);
-        $conference = $conferenceRepository->find($id);
+        $conference = $manager->getRepository(Conference::class)->find($id);
         $reservation =  new Reservation();
+        $form = $this->createForm(ReservationType::class, $reservation);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $reservation = $form->getData();
+            $reservation->setConference($conference);
+            $reservation->setUser($this->getUser());
+            
+            $res = $manager->getRepository(Reservation::class)->findBy(['user'=>$this->getUser(),'conference'=>$reservation->getConference()]);
+            // dd($res);
+            if( count($res) > 0){
+                $this->addFlash('failure','Vous avez déja réserver pour cette conference');
+                return $this->redirectToRoute('app_reservation',['id'=>$id]);
+            }
+            $manager->persist($reservation);
+            $manager->flush();
 
-        return $this->render('reservation/index.html.twig');
+            return $this->redirectToRoute('conference.index');  
+
+        }
+        return $this->render('reservation/index.html.twig',['form' => $form->createView()]);
     }
 }
