86008992ccbd4868a60a93356548755b6c3fb868
Mouhamed Gandega <mgandega@gmail.com>
2024-06-06 13:45:33 +0200

    envoie de mail via ajoutMessageListener

--- src/EventListener/AjoutConferenceListener.php
+++ src/EventListener/AjoutConferenceListener.php
@@ -2,14 +2,39 @@
 
 namespace App\EventListener;
 
+use Twig\Environment;
+use Symfony\Component\Mime\Email;
 use App\Events\AjoutConferenceEvent;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
 
 final class AjoutConferenceListener
 {
+    public function __construct(
+        public MailerInterface $mailer,
+        public Environment $twig,
+    ) {
+    }
+    // AjoutConferenceEvent::class => 'App\Events\AjoutConferenceEvent'
     #[AsEventListener(event: AjoutConferenceEvent::class)]
     public function onAjoutConferenceEvent(AjoutConferenceEvent $event): void
     {
-        dd('je suis un écouteur de cette évênement');
+
+        $email = $event->user->getEmail();
+        $pseudo = $event->user->getFirstname();
+        $message = $event->conference->getDescription();
+        // Génération du contenu HTML avec Twig
+        $htmlContent = $this->twig->render('contact\mailer.html.twig', [
+            'message' => $message,
+            'name' => $pseudo,
+        ]);
+        $email = (new Email())
+            ->from($email)
+            ->to('admin@monsite.com')
+            ->subject('Time for Symfony Mailer!')
+            ->text($message)
+            ->html($htmlContent);
+
+        $this->mailer->send($email);
     }
 }
